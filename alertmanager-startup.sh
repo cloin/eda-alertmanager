f52b8b41c3e070b06c6b7aa4ef1e021fcb0c8003
Colin McNaughton <cloin@users.noreply.github.com>
2023-07-17 15:18:53 -0400

    bash to sh

--- alertmanager-startup.sh
+++ alertmanager-startup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Create a new webhook on webhook.site container
 export WEBHOOK_URL=`curl -X POST -H "Content-Type: application/json" -d '{"default_status": 200, "default_content": "Hello world!", "default_content_type": "application/json", "timeout": 0, "cors": false, "expiry": true, "alias": "webhook", "actions": true}' http://podman:8084/token | jq -r '.uuid'`
