8086b14b367d1996fd07b21ffb653062f5dd3547
Colin McNaughton <cloin@users.noreply.github.com>
2023-07-17 15:00:10 -0400

    fix paths

--- alertmanager-startup.sh
+++ alertmanager-startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Create a new webhook on webhook.site container
-export WEBHOOK_UUID=`curl -X POST -H "Content-Type: application/json" -d '{"default_status": 200, "default_content": "Hello world!", "default_content_type": "application/json", "timeout": 0, "cors": false, "expiry": true, "alias": "webhook", "actions": true}' http://podman:8084/token | jq -r '.uuid'`
+export WEBHOOK_URL=`curl -X POST -H "Content-Type: application/json" -d '{"default_status": 200, "default_content": "Hello world!", "default_content_type": "application/json", "timeout": 0, "cors": false, "expiry": true, "alias": "webhook", "actions": true}' http://podman:8084/token | jq -r '.uuid'`
 
 # Substitute webhook UUID environment variable and generate final configuration file
-envsubst < alertmanager.yml > alertmanager.yml
+envsubst < /alertmanager.yml.template > /etc/alertmanager/alertmanager.yml
 
 # Start Alertmanager with the generated configuration
-./alertmanager --config.file=./alertmanager.yml
+/alertmanager --config.file=/etc/alertmanager/alertmanager.yml
