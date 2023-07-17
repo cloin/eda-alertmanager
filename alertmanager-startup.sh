80d60abd419401f30c8e467a97147fe7ed593910
Colin McNaughton <cloin@users.noreply.github.com>
2023-07-17 16:52:49 -0400

    docs

--- alertmanager-startup.sh
+++ alertmanager-startup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# this script creates a new webhook receiver on the webhook container and uses that 
+# in the alertmanager.yml file to send alerts to a webhook
+
 # Create a new webhook on webhook.site container
 export WEBHOOK_UUID=`curl -X POST -H "Content-Type: application/json" -d '{"default_status": 200, "default_content": "Hello world!", "default_content_type": "application/json", "timeout": 0, "cors": false, "expiry": true, "alias": "webhook", "actions": true}' http://podman:8084/token | jq -r '.uuid'`
 
